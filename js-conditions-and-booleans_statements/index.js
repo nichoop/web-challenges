a26b9a2e28339ecf2d18be8e4548a9f9fef0773a
Nicholas Ntiri <ntiri97@gmail.com>
2024-04-18 10:50:32 +0200

    nuovo

---
+++ js-conditions-and-booleans_statements/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+const receivedPassword = "password1234";
+
+if (receivedPassword !== SUPER_SECRET_PASSWORD )
+ {console.log("Access denied")};
+
+ else if(receivedPassword === SUPER_SECRET_PASSWORD)
+ {
+    console.log("Welcome! You are logged in as Brunhilde1984.")
+};
+
+
+// Part 2: Even / Odd
+const number = 6;
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = "";
+
+console.log(statement);
+
+// Part 5: Greeting
+const userName = "Archibald";
+
+const greeting = "Hello " + "//enter your code here" + "!";
+
+console.log(greeting);
